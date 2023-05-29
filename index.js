098120f0345959bd2394eff047d93276a8ae54f3
eulyoungjung <jyy6237@naver.com>
2023-05-30 00:03:34 +0900

    modify index.js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const sharp = require('sharp');
 import { ErrorRequestHandler } from 'express';
-const { S3Client } = require('@aws-sdk/client-s3');
+const { S3Client, GetObjectCommand, PutObjectCommand } = require('@aws-sdk/client-s3');
 
 const s3 = new S3Client();  //  여기 js 페이지가 람다에서 실행될 것인데  람다에서 S3Client의 빈곳에 access_key 랑 secret_access_key를  알아서 넣어 준다 . 
 
@@ -15,18 +15,23 @@ exports.handler = async (event, context, callback) => {
     console.log('name', filename, 'ext', ext);
 
     try {
-        const s3Object = await s3.getObject({ Bucket: Key });
-        console.log('origianl', s3Object);
-        const resizedImage = await sharp(s3Object.body)
+        const getObject = await s3.send(new GetObjectCommand({ Bucket: Key }));
+        const buffers = [];
+        for await (const data of getObject.Body) {
+            buffers.push(data);
+        }
+        const imageBuffer = Buffer.concat(buffers);
+        console.log('put', imageBuffer.length);
+        const resizedImage = await sharp(buffers)
             .resize(200, 200, { fit: 'inside' })  //200 * 200 사이즈안에서 최대한 크기 꽉 맞춰서 리사이징을 한다 . 
             .toFormat(requiredFormat)
             .toBuffer();
-        await s3.putObject({  //다시 s3 에 저장 하는것 입니다       
+        await s3.send(new PutObjectCommand({  //다시 s3 에 저장 하는것 입니다       
             Bucket,
             Key: `thumb/${filename}`, // thumb/고양이.png
             Body: resizedImage,
-        })
-        console.log('put', resizedImage, length);
+        }))
+        console.log('put', resizedImage.length);
         return callback(null, `thumb/${filename}`);
     } catch (error) {
         console.error(error);
